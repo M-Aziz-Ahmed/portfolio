b915c18c92b7f7bc0aabf39483f58f05a87a398c
Aziz Ahmed <azizahmed10088@gmail.com>
2024-10-31 21:11:01 +0500

    ui

--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -1,12 +1,14 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import SideBar from "./components/sidebar/SideBar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className=''
       >
+        <SideBar/>
         {children}
       </body>
     </html>
