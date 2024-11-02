c95e4149b2a69533f72acb882e88f232128754f9
Aziz Ahmed <azizahmed10088@gmail.com>
2024-11-03 00:13:47 +0500

    ui

--- my-portfolio/app/components/pages/Home.jsx
+++ my-portfolio/app/components/pages/Home.jsx
@@ -1,8 +1,19 @@
+import Image from "next/image"
 const Home = ({}) => {
   return (
     <>
-    <div className="h2 text-light">
-        Hello!, My Name Is Syed Muhammad Aziz Ahmed
+    <div className="container">
+      <div className="d-flex w-100 flex-column my-5 justify-content-end">
+        <div className="profile rounded-circle">
+          <Image src='/profile.jpg' height={300} width={300} className="profile-pic" alt="Error Loading Image"></Image>
+        </div>
+        <div className="text-body my-5">
+          <div className="h1 text-light">~~ Ohh, You Finally Landed Here ~~</div>
+          <hr className="text-light"/>
+          <div className="h3 text-light">I am Aziz and this Site explains " Me "</div>
+
+        </div>
+      </div>
     </div>
     </>
   )
