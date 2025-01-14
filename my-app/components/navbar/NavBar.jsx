d2f3857e2419af603a5b210f83b63a174d8923f9
Syed Muhammad Aziz Ahmed <azizahmed10088@gmail.com>
2025-01-14 17:31:06 +0500

    ui

--- my-app/components/navbar/NavBar.jsx
+++ my-app/components/navbar/NavBar.jsx
@@ -2,18 +2,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa';  // Font Awesome icons
+import { SiNextdotjs, SiDjango, SiFigma } from 'react-icons/si'; // Other useful icons
 
 const NavBar = () => {
   const [theme, setTheme] = useState(true);
   const [activeLink, setActiveLink] = useState('#profile');  // Track active link
 
   const NavLinks = [
-    { title: 'Profile', link: '#profile' },
-    { title: 'Skills', link: '#skills' },
-    { title: 'Experience', link: '#exp' },
-    { title: 'Certifications', link: '#cert' },
-    { title: 'Education', link: '#edu' },
-    { title: 'Contact Me', link: 'https://wa.me/message/VI3BD7PEX4PWF1' },
+    { icon: <FaWhatsapp className="text-success display-6"/>, link: 'https://wa.me/message/VI3BD7PEX4PWF1' },
+    { icon: <FaInstagram className="text-danger display-6"/>, link: 'https://www.instagram.com/azizahmed_2nd/' },
   ];
 
   const handleLinkClick = (link) => {
@@ -23,11 +21,12 @@ const NavBar = () => {
   return (
     <div className={`${theme ? 'bg-white' : 'bg-dark'} w-100 h-100`}>
       <div className="d-flex flex-column align-items-center">
-        <div className="logo text-center rounded-circle" style={{ overflow: 'hidden', height: '250px', width: '250px' }}>
+        <div className="logo text-center rounded-circle my-2" style={{ overflow: 'hidden', height: '200px', width: '200px' }}>
           <Image src='/logo.jpg' height={300} width={300} alt="Logo" />
         </div>
         <hr />
-        <div className="d-flex flex-column gap-3 w-100">
+        <div className="display-6 my-3 text-slate-600">Connect With Me</div>
+        <div className="d-flex align-items-center justify-center bg-slate-300 p-2 gap-3 w-100">
           {NavLinks.map((nav, id) => (
             <div
               className={`nav-item py-3 px-2 rounded text-center ${activeLink === nav.link ? 'bg-slate-200' : ''}`}  // Highlight active link
@@ -36,9 +35,10 @@ const NavBar = () => {
               <Link
                 className={`nav-link ${theme ? (activeLink === nav.link ? 'text-primary' : '') : (activeLink === nav.link ? 'text-primary' : 'text-light')}`}
                 href={nav.link}
+                target="blank"
                 onClick={() => handleLinkClick(nav.link)}  // Update active link on click
               >
-                {nav.title}
+                {nav.icon}
               </Link>
             </div>
           ))}
